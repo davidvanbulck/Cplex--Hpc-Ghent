e888df55f6092aeaadc71211c1ce8a8f177b2d9b
vsc41980 vscuser <vsc41980@gligar08.gastly.os>
2024-10-25 14:14:59 +0200

    New cplex version

--- runScheduling.sh
+++ runScheduling.sh
@@ -4,21 +4,19 @@
 
 # 1 nodes, 1 processor (note: processor = core)
 # Note: do not forget to limit number of threads Gurobi can use (otherwise Gurobi thinks it has all processors available on the cluster whereas it has only one available)
-#PBS -l  nodes=1:ppn=1
+#PBS -l nodes=1:ppn=1
 
 # Expected running time 5 seconds
 #PBS -l walltime=0:00:05
 
-# Use infiniband nodes (important when you use mulitple cores)
-#PBS -l ib
-
 # Move to the working directory
 # Alternatively: $ cd  /user/gent/419/vsc41980/gurobi/sudoku_code
-cd ~/Cplex--HPC-Ghent
+cd ~/Cplex--Hpc-Ghent
 
 # Load modules -> go to golett: cplex not available on all clusters
-module swap cluster/golett
-module load CPLEX/12.6
+module swap cluster/doduo
+#module load CPLEX/12.6
+module load CPLEX/22.1.1-GCCcore-11.2.0
 
 # Compile the program
 make scheduling
